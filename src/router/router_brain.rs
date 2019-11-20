0e933a934dfe8c9b4f7e67f0077161cec25c2a8b
yujinliang <285779289@qq.com>
2019-11-20 21:19:15 +0800

    refactor router mod

--- src/router/router_brain.rs
+++ src/router/router_brain.rs
@@ -4,21 +4,22 @@ use std::collections::HashMap;
 
 pub struct Router <'a>{
 
-    cfg : &'a Config,
+    cfg : Option<&'a Config>,
+    r_table : Option< RouterTable<'a>>,
 }
 
 impl<'a> Router<'a> {
 
    pub  fn new( cfg: &Config) -> Router {
-       Router{cfg}
+       Router{cfg : Some(cfg), r_table : None}
     }
 
   pub   fn gen_routing_table(&mut self) -> Result<String ,String > {
 
-        let r_table = RouterTable{
+        self.r_table = Some(RouterTable{
             table_entry:HashMap::new(),
-        };
-        let s = format!("{:?}",  r_table);
+        });
+        let s = format!("{:?}",  self.r_table);
         Ok(s)
     }
 }
